e5aaeef4fa6b9225e6f05e341aa15bac032ad096
Alvian Zachry <alvian.faturrahman@webimp.com.sg>
2019-10-18 17:34:18 +0700

    optional query string access

--- src/main/java/com/alvianzf/app/ws/ui/controllers/UserController.java
+++ src/main/java/com/alvianzf/app/ws/ui/controllers/UserController.java
@@ -6,12 +6,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("users") //http://localhost:8080/users
 public class UserController {
 
+	@GetMapping()
+	public String getUsers(@RequestParam(value="page", defaultValue="1", required=false) int page,
+			@RequestParam(value="limit", defaultValue="50") int limit,
+			@RequestParam(value="sort", required=false, defaultValue="desc") String sort)
+	{
+		return "getUser was called with page = " + page + " and limit = " + limit ;
+	}
+	
 	@GetMapping(path="/{userId}")
 	public String getUser(@PathVariable String userId)
 	{
